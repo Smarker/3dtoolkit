b4c5d2466ad7f02363a864d4e1bf41e4a49dcd2e
Arman <armanrahman22@gmail.com>
2017-09-06 16:55:43 -0400

    added server list activity

--- Samples/Client/AndroidNative/3dToolkitAndroid/app/src/main/java/microsoft/a3dtoolkitandroid/Connect.java
+++ Samples/Client/AndroidNative/3dToolkitAndroid/app/src/main/java/microsoft/a3dtoolkitandroid/Connect.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class Connect extends AppCompatActivity {
+    public static final String SERVER_LIST = "com.microsoft.a3dtoolkitandroid.LIST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +30,48 @@ public class Connect extends AppCompatActivity {
      * @param view
      */
     public void connect(View view) {
-        Intent intent = new Intent(this, Stream.class);
-        String server = ((EditText) findViewById(R.id.server)).getText().toString().toLowerCase();
+        final Intent intent = new Intent(this, ServerList.class);
+        final String server = ((EditText) findViewById(R.id.server)).getText().toString().toLowerCase();
         String port = ((EditText) findViewById(R.id.port)).getText().toString().toLowerCase();
+
+        //alert dialog
+        final AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        //check for empty
         if (server.length() == 0 || port.length() == 0) {
-            AlertDialog.Builder builder;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
-            } else {
-                builder = new AlertDialog.Builder(this);
-            }
             builder.setTitle("Missing Info")
                     .setMessage(server.length() == 0 ? "Please fill server" : "Please fill port");
+        } else {
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = server + "/sign_in?peer_name=" + port;
+
+            // Request a string response from the server
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            intent.putExtra(SERVER_LIST, response);
+                            startActivity(intent);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    builder.setTitle("Error")
+                            .setMessage("Sorry request did not work!");
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+
+            //show the alert
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
-        } else {
-            startActivity(intent);
         }
     }
 }
---
+++ Samples/Client/AndroidNative/3dToolkitAndroid/app/src/main/java/microsoft/a3dtoolkitandroid/ServerList.java
@@ -0,0 +1,44 @@
+package microsoft.a3dtoolkitandroid;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.R.id.list;
+
+public class ServerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_list);
+        final ListView listview = (ListView) findViewById(R.id.ServerListView);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        List<String> serverListString = new ArrayList<String>(Arrays.asList(intent.getStringExtra(Connect.SERVER_LIST).split("\n")));
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, serverListString);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                //todo: do action with chosen item
+            }
+        });
+
+    }
+}
